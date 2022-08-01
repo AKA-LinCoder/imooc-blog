02e5a6097e1523fb582dabfea9e97870df67f117
AKA-LinCoder <2423860442@qq.com>
2022-08-01 16:35:58 +0800

    样式美化

--- components/hot-list-item/hot-list-item.vue
+++ components/hot-list-item/hot-list-item.vue
@@ -45,5 +45,54 @@
 </script>
 
 <style lang="scss" scoped>
+.item-container {
+  padding-bottom: $uni-spacing-col-lg;
+  .item-box {
+    display: flex;
+    margin: 0 $uni-spacing-col-base;
+    padding: $uni-spacing-row-lg $uni-spacing-col-base;
+    background-color: $uni-bg-color;
+    border-radius: $uni-border-radius-lg;
+    box-shadow: 2px 2px 5px 1px rgba(143, 143, 143, 0.1);
+    .item-box-left {
+      margin-right: $uni-spacing-row-sm;
+    }
+    .item-box-right {
+      width: 100%;
+      .item-title {
+        font-size: $uni-font-size-lg;
+        font-weight: bold;
+        color: $uni-text-color-title;
+      }
+      .item-desc {
+        padding-top: $uni-spacing-row-sm;
+        font-size: $uni-font-size-base;
+        color: $uni-text-color;
+      }
+      .item-bottom-box {
+        margin-top: $uni-spacing-col-sm;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .item-author {
+          font-size: $uni-font-size-sm;
+          color: $uni-text-color-grey;
+        }
+        .hot-box {
+          .hot-icon {
+            width: $uni-img-size-sm;
+            height: $uni-img-size-sm;
+            vertical-align: top;
+          }
+          .hot-text {
+            margin-left: $uni-spacing-row-sm;
+            font-size: $uni-font-size-sm;
+            color: $uni-text-color-hot;
+          }
+        }
+      }
+    }
+  }
+}
 
 </style>
--- components/hot-ranking/hot-ranking.vue
+++ components/hot-ranking/hot-ranking.vue
@@ -1,6 +1,6 @@
 <template>
-	<view class="ranking-container">
-		<image class="rangking-bg" src="../../static/images/ranking-1.png">
+	<view class="ranking">
+		<image class="ranking-bg" src="../../static/images/ranking-1.png">
 			
 		</image>
 		<text class="ranking-text">1</text>
@@ -18,6 +18,27 @@
 	}
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.ranking {
+  position: relative;
+  text-align: center;
+  width: 18px;
+  height: 20px;
+  .ranking-bg {
+    width: 100%;
+    height: 100%;
+  }
+  .ranking-text {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-55%, -55%);
+    font-size: $uni-font-size-sm;
+    font-weight: bold;
+    color: $uni-text-color;
+  }
+  .text-white {
+    color: white;
+  }
+}
 </style>
